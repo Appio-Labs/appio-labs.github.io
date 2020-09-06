16d4337ec224e111d3fdb53fb9fdbcdeac5a7a5f
Raul Jareño <raul.jareno.diaz@gmail.com>
2020-09-06 15:11:07 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run generate
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'appio.site' > CNAME
+
+cd ..
+
+# if you are deploying to https://<USERNAME>.github.io
+npm run deploy
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
