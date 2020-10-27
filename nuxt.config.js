5ccbc5cb9632f0c2f40f85413c905cb8f30f115e
Raul Jareño <raul.jareno.diaz@gmail.com>
2020-10-27 15:07:40 +0100

    fixed nuxt firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,8 +38,7 @@ export default {
    */
   plugins: [
   '~plugins/sofbox.js',
-  '~plugins/i18n.js',
-  '~plugins/firebase.js'
+  '~plugins/i18n.js'
   ],
 
   target: 'static',
@@ -63,8 +62,24 @@ export default {
     // Doc: https://bootstrap-vue.js.org/docs/
     'bootstrap-vue/nuxt',
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/firebase',
   ],
+  firebase: {
+    config: {
+      apiKey: "AIzaSyCR5CwdgEmhgSU7kV6lcQnvjqTziI2s35s",
+      authDomain: "appio-labs.firebaseapp.com",
+      databaseURL: "https://appio-labs.firebaseio.com",
+      projectId: "appio-labs",
+      storageBucket: "appio-labs.appspot.com",
+      messagingSenderId: "314386062516",
+      appId: "1:314386062516:web:c31c7417a3872591874be1",
+      measurementId: "G-GKV76DETS0"
+    },
+    services: {
+      analytics: true
+    }
+  },
   /*
    ** Build configuration
    */
