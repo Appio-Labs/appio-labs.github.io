58d176edb35d0a45acfbd94ba70823b40cb709b7
Raul Jareño <raul.jareno.diaz@gmail.com>
2020-12-18 11:36:58 +0100

    fixed comfortaa font

--- components/Sofbox-sass/Slider.vue
+++ components/Sofbox-sass/Slider.vue
@@ -1,7 +1,22 @@
 <template>
-  <div id="rev_slider_24_1_wrapper" class="rev_slider_wrapper fullwidthbanner-container" data-alias="saas-dark-version" style="margin:0px auto;background-color:transparent;padding:0px;margin-top:0px;margin-bottom:0px;">
+  <div
+    id="rev_slider_24_1_wrapper"
+    class="rev_slider_wrapper fullwidthbanner-container"
+    data-alias="saas-dark-version"
+    style="
+      margin: 0px auto;
+      background-color: transparent;
+      padding: 0px;
+      margin-top: 0px;
+      margin-bottom: 0px;
+    "
+  >
     <!-- START REVOLUTION SLIDER 5.2.6 fullwidth mode -->
-    <div id="rev_slider_24_1" class="rev_slider fullwidthabanner tp-overflow-hidden" data-version="5.2.6">
+    <div
+      id="rev_slider_24_1"
+      class="rev_slider fullwidthabanner tp-overflow-hidden"
+      data-version="5.2.6"
+    >
       <ul>
         <!-- SLIDE  -->
         <li
@@ -38,57 +53,72 @@
             data-bgparallax="off"
             class="rev-slidebg"
             data-no-retina
-          >
+          />
           <!-- LAYERS -->
 
           <!-- LAYER NR. 1 -->
           <h1
             id="slide-21-layer-1"
-            class="tp-caption   tp-resizeme"
+            class="tp-caption tp-resizeme"
             data-x="49"
             data-y="246"
             data-width="['auto']"
             data-height="['auto']"
             data-transform_idle="o:1;"
-
             data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1000;e:Power3.easeInOut;"
             data-transform_out="opacity:0;s:300;e:Power3.easeInOut;"
             data-start="900"
             data-splitin="none"
             data-splitout="none"
             data-responsive_offset="on"
-
-            style="z-index: 5; white-space: nowrap; font-size: 50px; line-height: 70px; font-weight: 700; color: rgba(255, 255, 255, 1.00);font-family:Montserrat;"
+            style="
+              z-index: 5;
+              white-space: nowrap;
+              font-size: 50px;
+              line-height: 70px;
+              font-weight: 700;
+              color: rgba(255, 255, 255, 1);
+              font-family: Montserrat;
+            "
           >
-            Best Ever Saas Landing <br>Template
+            Best Ever Saas Landing <br />Template
           </h1>
 
           <!-- LAYER NR. 2 -->
           <p
             id="slide-21-layer-2"
-            class="tp-caption   tp-resizeme"
+            class="tp-caption tp-resizeme"
             data-x="50"
             data-y="430"
             data-width="['auto']"
             data-height="['auto']"
             data-transform_idle="o:1;"
-
             data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1000;e:Power3.easeInOut;"
             data-transform_out="opacity:0;s:300;e:Power3.easeInOut;"
             data-start="1200"
             data-splitin="none"
             data-splitout="none"
             data-responsive_offset="on"
-
-            style="z-index: 6; white-space: nowrap; font-size: 16px; line-height: 30px; font-weight: 400; color: rgba(224, 224, 224, 1.00);font-family:Muli;"
+            style="
+              z-index: 6;
+              white-space: nowrap;
+              font-size: 16px;
+              line-height: 30px;
+              font-weight: 400;
+              color: rgba(224, 224, 224, 1);
+              font-family: Muli;
+            "
           >
-            here are many variations of passages of Lorem Ipsum available, but the majority have <br>suffered alteration in some form, by injected humour, or randomised words<br>which don't look even slightly believable.
+            here are many variations of passages of Lorem Ipsum available, but
+            the majority have <br />suffered alteration in some form, by
+            injected humour, or randomised words<br />which don't look even
+            slightly believable.
           </p>
 
           <!-- LAYER NR. 3 -->
           <div
             id="slide-21-layer-3"
-            class="tp-caption button  tp-resizeme"
+            class="tp-caption button tp-resizeme"
             data-x="50"
             data-y="570"
             data-width="['auto']"
@@ -96,15 +126,31 @@
             data-transform_idle="o:1;"
             data-transform_hover="o:1;rX:0;rY:0;rZ:0;z:0;s:0;e:Linear.easeNone;"
             data-style_hover="c:rgba(0, 0, 0, 1.00);bg:rgba(255, 255, 255, 1.00);"
-
             data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1000;e:Power3.easeInOut;"
             data-transform_out="opacity:0;s:300;e:Power3.easeInOut;"
             data-start="1500"
             data-splitin="none"
             data-splitout="none"
             data-responsive_offset="on"
-
-            style="z-index: 7; white-space: nowrap; font-size: 17px; line-height: 17px; font-weight: 500; color: rgba(255, 255, 255, 1.00);font-family:Roboto;background-color:rgba(0, 0, 0, 0.75);padding:12px 35px 12px 35px;border-color:rgba(0, 0, 0, 1.00);border-radius:5px 5px 5px 5px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;cursor:pointer;"
+            style="
+              z-index: 7;
+              white-space: nowrap;
+              font-size: 17px;
+              line-height: 17px;
+              font-weight: 500;
+              color: rgba(255, 255, 255, 1);
+              font-family: Roboto;
+              background-color: rgba(0, 0, 0, 0.75);
+              padding: 12px 35px 12px 35px;
+              border-color: rgba(0, 0, 0, 1);
+              border-radius: 5px 5px 5px 5px;
+              outline: none;
+              box-shadow: none;
+              box-sizing: border-box;
+              -moz-box-sizing: border-box;
+              -webkit-box-sizing: border-box;
+              cursor: pointer;
+            "
           >
             Read More
           </div>
@@ -112,19 +158,17 @@
           <!-- LAYER NR. 4 -->
           <div
             id="slide-21-layer-4"
-            class="tp-caption   tp-resizeme rs-parallaxlevel-1"
+            class="tp-caption tp-resizeme rs-parallaxlevel-1"
             data-x="710"
             data-y="111"
             data-width="['none','none','none','none']"
             data-height="['none','none','none','none']"
             data-transform_idle="o:1;"
-
             data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1000;e:Power3.easeInOut;"
             data-transform_out="opacity:0;s:300;e:Power3.easeInOut;"
             data-start="1800"
             data-responsive_offset="on"
-
-            style="z-index: 8;"
+            style="z-index: 8"
           >
             <div
               class="rs-looped rs-slideloop"
@@ -135,17 +179,27 @@
               data-ys="0"
               data-ye="0"
             >
-              <img src="../../assets/sofbox-sass-black/revslider/assets/881cd-01.png" alt="" data-ww="auto" data-hh="auto" data-no-retina>
+              <img
+                src="../../assets/sofbox-sass-black/revslider/assets/881cd-01.png"
+                alt=""
+                data-ww="auto"
+                data-hh="auto"
+                data-no-retina
+              />
             </div>
           </div>
         </li>
       </ul>
-      <div class="tp-bannertimer tp-bottom" style="visibility: hidden !important;" />
+      <div
+        class="tp-bannertimer tp-bottom"
+        style="visibility: hidden !important"
+      />
     </div>
-  </div><!-- END REVOLUTION SLIDER -->
+  </div>
+  <!-- END REVOLUTION SLIDER -->
 </template>
 <script>
 export default {
-  name: 'Slider'
-}
+  name: "Slider",
+};
 </script>
