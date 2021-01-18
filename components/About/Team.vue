cb397c362f3e43e4d7262f26e99d817509ad1e08
Raul Jareño <raul.jareno.diaz@gmail.com>
2021-01-18 13:04:51 +0100

    added hover to meet the team

--- components/About/Team.vue
+++ components/About/Team.vue
@@ -4,26 +4,36 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-12">
-            <div class="heading-title">
-              <h3 class="title iq-tw-7">{{ $t("about.team.title") }}</h3>
-               <p>{{ $t("about.team.description") }}</p>
-            </div>
+          <div class="heading-title">
+            <h3 class="title iq-tw-7">{{ $t("about.team.title") }}</h3>
+            <p>{{ $t("about.team.description") }}</p>
           </div>
+        </div>
       </div>
       <div class="row">
-        <div v-for="(list,index) in temeList" :key="index" class="col-lg-6 col-sm-6 iq-mb-30">
+        <div
+          v-for="(list, index) in temeList"
+          :key="index"
+          class="col-lg-6 col-sm-6 iq-mb-30"
+        >
           <CardStyle3>
             <div slot="cardMedia">
-              <img alt="" class="img-fluid" :src="list.media">
+              <img
+                alt=""
+                class="img-fluid"
+                :src="list.image"
+                @mouseover="list.image = list.hoverMedia"
+                @mouseleave="list.image = list.media"
+              />
             </div>
             <div slot="cardTitle">
               <!--<a href="team-details-1.html">-->
-                <h6 class="iq-tw-6">{{ list.title }}</h6>
+              <h6 class="iq-tw-6">{{ list.title }}</h6>
               <!--</a>-->
               <span class="team-post">{{ list.subtitle }}</span>
             </div>
             <ul slot="cardFooter" class="list-inline">
-              <li v-for="(social,sindex) in list.socialLinks" :key="sindex">
+              <li v-for="(social, sindex) in list.socialLinks" :key="sindex">
                 <a :href="social.link"> <i :class="social.icon" /> </a>
               </li>
             </ul>
@@ -36,40 +46,57 @@
 </template>
 
 <script>
-import CardStyle3 from '../sofbox/Card/CardStyle3'
+import CardStyle3 from "../sofbox/Card/CardStyle3";
 export default {
-  name: 'Team',
+  name: "Team",
   components: { CardStyle3 },
-  data () {
+  data() {
     return {
       temeList: [
         {
-          media: require('../../assets/images/team/small/01.jpg'),
-          title: 'Raúl Jareño',
-          subtitle: 'CEO, Appio',
+          image: require("../../assets/images/team/small/01-1.png"),
+          media: require("../../assets/images/team/small/01-1.png"),
+          hoverMedia: require("../../assets/images/team/small/01.jpg"),
+          title: "Raúl Jareño",
+          subtitle: "CEO, Appio",
           socialLinks: [
-            { icon: 'fab fab-team fa-linkedin', link: '#' },
-            { icon: 'fab fab-team fa-github', link: '#' },
+            {
+              icon: "fab fab-team fa-linkedin",
+              link: "https://www.linkedin.com/in/rauljareno/",
+            },
+            {
+              icon: "fab fab-team fa-github",
+              link: "https://github.com/rauljareno",
+            },
             /*{ icon: 'fab fa-youtube', link: '#' }*/
-          ]
+          ],
         },
         {
-          media: require('../../assets/images/team/small/02.jpg'),
-          title: 'Patricia Gruart',
-          subtitle: 'Sales Manager, Appio',
+          image: require("../../assets/images/team/small/02-2.png"),
+          media: require("../../assets/images/team/small/02-2.png"),
+          hoverMedia: require("../../assets/images/team/small/02.jpg"),
+          title: "Patricia Gruart",
+          subtitle: "Sales Manager, Appio",
           socialLinks: [
-            
-            { icon: 'fab fab-team fa-linkedin', link: '#' },
-            { icon: 'fab fab-team fa-instagram', link: '#' },
-            { icon: 'fab fab-team fa-facebook', link: '#' },
-          ]
-        }
-      ]
-    }
-  }
-}
+            {
+              icon: "fab fab-team fa-linkedin",
+              link: "https://www.linkedin.com/in/patty-gruart-68aab225/",
+            },
+            {
+              icon: "fab fab-team fa-instagram",
+              link: "https://www.instagram.com/pattygruart/",
+            },
+            {
+              icon: "fab fab-team fa-facebook",
+              link: "https://www.facebook.com/HastalaModa",
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
